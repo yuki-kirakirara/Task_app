ad25fdee8d9294e7ffe874550e715ba3b2a046a6
yuki.kirakirara <da1am0nd0.ll.tt@gmail.com>
2021-06-02 06:16:15 +0000

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,9 @@ ActiveRecord::Schema.define(version: 2021_05_31_045837) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
-    t.string "name"
-    t.string "email"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end

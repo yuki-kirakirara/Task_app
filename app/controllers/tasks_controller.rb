3b5629a9538031cbc5bff6c01e268881b7ae8697
yuki.kirakirara <da1am0nd0.ll.tt@gmail.com>
2021-06-02 04:51:44 +0000

    re-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,10 +4,10 @@ class TasksController < ApplicationController
    before_action :correct_user, only: [:show, :edit, :destroy, :update]
   
     def index
-       if logged_in?
-      @task= current_user.tasks.build  # form_with 用
-      @tasks= current_user.tasks.order(id: :desc).page(params[:page]).per(25)
-       end
+       #if logged_in?
+      #@task= current_user.tasks.build  # form_with 用
+      @tasks= current_user.tasks.order(id: :desc).page(params[:page]).per(10)
+       #end
     end
    
     def show
@@ -18,14 +18,14 @@ class TasksController < ApplicationController
     end
     
     def create
-      #@task = Task.new(task_params)
+      @task = Task.new(task_params)
       
        @task = current_user.tasks.build(task_params)
       if @task.save
         flash[:success] = 'タスクが投稿されました'
         redirect_to @task
       else
-         @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+         #@tasks = current_user.tasks.order(id: :desc).page(params[:page])
         flash.now[:danger] = 'タスク投稿がされませんでした'
         render :new
         

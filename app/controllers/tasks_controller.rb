ed02b6baafbc35e69dec09ccac94d7b1d20510d1
yuki.kirakirara <da1am0nd0.ll.tt@gmail.com>
2021-06-04 03:48:32 +0000

    tasklist_re

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,47 +1,51 @@
 class TasksController < ApplicationController
    before_action :require_user_logged_in
-   before_action :correct_user, only: [:show, :edit, :destroy, :update]
+   before_action :correct_user, only: [:edit, :destroy, :update, :show,]
   
     def index
-      @tasks= current_user.tasks.order(id: :desc)
+       @tasks= current_user.tasks.order(id: :desc).page(params[:page]).per(5)
     end
+    
     def show
     end
+    
     
     def new
       @task = Task.new
     end
     
+    
     def create
-      @task = Task.new(task_params)
-      
-      if @task.save
+      @task = current_user.tasks.build(task_params)
+      if @task.save(task_params)
         flash[:success] = 'タスクが投稿されました'
-        redirect_to @task
+        redirect_to root_url
       else
         flash.now[:danger] = 'タスク投稿がされませんでした'
         render :new
       end
     end
     
+    
     def edit
     end
     
-    def update
+    
+   def update
       if @task.update(task_params)
         flash[:success] = 'タスクが編集されました'
-        redirect_to @task
+        redirect_to root_url
       else
         flash.now[:danger] = 'タスクが編集されませんでした'
         render :edit
       end
-    end
+   end
   
   
     def destroy
       @task.destroy
       flash[:success] = 'タスクが削除されました'
-      redirect_to @task
+      redirect_to root_url
     end
   
   
